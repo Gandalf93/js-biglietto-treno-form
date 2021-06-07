cf85685635056fc2816983bec762ee88dd83eb37
Giulia <giuliatullia93@gmail.com>
2021-06-07 14:06:02 +0200

    add-grafica-da-aggiustare

--- js/script.js
+++ js/script.js
@@ -3,45 +3,60 @@ var submit = document.getElementById('genera');
 submit.addEventListener('click', function(){
 
    var nomePass = document.getElementById('nome').value;
-   console.log('nome'); 
-   document.getElementById("passeggero").innerHTML = nomePass;
-
-   var km = document.getElementById('km').value;
-   console.log('km');
    
-
+   var km = document.getElementById('km').value;
+   
    var age = document.getElementById('fascia-eta').value;
-   console.log('età');
-   document.getElementById("offerta").innerHTML = age;
 
    var prezzo = km * 0.21
-   console.log('totale senza sconto', prezzo);
-   
-
+  
    var prezzoMinorenni = prezzo - prezzo * 20 / 100
-   console.log('prezzo scontato minorenni', prezzoMinorenni)
-
+  
    var prezzoOver65 = prezzo - prezzo * 40 / 100
-   console.log('prezzo scontato over 65', prezzoOver65)
-
+   
    var outcome = prezzo
 
    if (age == 'minorenne') {
     var outcome = prezzoMinorenni
-    console.log('prezzo scontato minorenni');
+    
    } else if (age == 'over65') { 
     var outcome = prezzoOver65
-    console.log('prezzo scontato over 65');
+    
    }
 
    var outcome = outcome.toFixed(2);
+   var carrozza = Math.floor(Math.random() * 9) + 1;
+   var codCp = Math.floor(Math.random() * 10000) + 90000;
+
+   document.getElementById('biglietto').className = 'show';
    
+   document.getElementById("passeggero").innerHTML = nomePass;
+   document.getElementById("offerta").innerHTML = age;
+   document.getElementById("carrozza").innerHTML = carrozza;
+   document.getElementById("codiceCp").innerHTML = codCp;
    document.getElementById("prezzo").innerHTML = outcome;
 
 })
    
 var annulla = document.getElementById('annulla');
 
-annulla.addEventListener('annulla', function(){
-    
+annulla.addEventListener('click', function(){
+    document.getElementById('nome').value = '';
+    document.getElementById('km').value = '';
+    document.getElementById('fascia-eta').value = '';
+
+    document.getElementById('passeggero').className = 'hidden';
+    document.getElementById('offerta').className = 'hidden';
+    document.getElementById('carrozza').className = 'hidden';
+    document.getElementById('codiceCp').className = 'hidden';
+    document.getElementById('prezzo').className = 'hidden';
 })
+
+
+   
+  
+
+   
+   
+
+   
